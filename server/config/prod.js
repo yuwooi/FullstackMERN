da2268b6be4fe5b74a84fd5444d70ea7a58928b3
Ooi Yu Wooi <yuwooi@Oois-MacBook-Pro.local>
2020-02-27 12:48:09 +0800

    finished OAuth flow

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleCLientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGODB_URI,
+  cookiekey: process.env.COOKIE_KEY
+};
