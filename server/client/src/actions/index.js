b870bc790e8b53ae11f6d7fd265df61c99a8d30d
Ooi Yu Wooi <yuwooi@Oois-MacBook-Pro.local>
2020-02-28 16:38:06 +0800

    Added billing and client side app

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -5,3 +5,8 @@ export const fetchUser = () => async dispatch => {
   const response = await axios.get("/api/current_user");
   dispatch({ type: actionTypes.FETCH_USER, payload: response.data });
 };
+
+export const handleToken = token => async dispatch => {
+  const response = await axios.post("/api/stripe", token);
+  dispatch({ type: actionTypes.FETCH_USER, payload: response.data });
+};
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
@@ -15,9 +16,17 @@ class Header extends Component {
         );
       default:
         return (
-          <li>
-            <a href='/api/logout'>Logout</a>
-          </li>
+          <>
+            <li>
+              <Payments />
+            </li>
+            <li style={{ margin: "0 10px" }}>
+              Credits: {this.props.auth.credits}
+            </li>
+            <li>
+              <a href='/api/logout'>Logout</a>
+            </li>
+          </>
         );
     }
   }
